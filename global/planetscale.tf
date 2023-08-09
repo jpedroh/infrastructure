d3f59c0c435a34fdedc6fa8146327641bef435cc
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-08-09 19:40:21 -0300

    fix: Add workaround to address empty password

--- global/planetscale.tf
+++ global/planetscale.tf
@@ -1,8 +1,18 @@
+resource "null_resource" "always_run" {
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
+}
+
 resource "planetscale_database_branch_password" "production" {
   organization = local.planetscale_service_organization
   database     = local.database_name
   branch       = local.database_production_branch_name
   name         = "${local.database_production_branch_name}-terraform"
+
+  lifecycle {
+    replace_triggered_by = [null_resource.always_run]
+  }
 }
 
 resource "planetscale_database_branch_password" "preview" {
@@ -10,4 +20,8 @@ resource "planetscale_database_branch_password" "preview" {
   database     = local.database_name
   branch       = local.database_preview_branch_name
   name         = "${local.database_preview_branch_name}-terraform"
+  
+  lifecycle {
+    replace_triggered_by = [null_resource.always_run]
+  }
 }
