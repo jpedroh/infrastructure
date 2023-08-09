bda2d8c041381870f6f6da71a848d44dc52b43ef
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-08-09 20:36:11 -0300

    feat: add declaration of infra repository

--- global/github.tf
+++ global/github.tf
@@ -1,3 +1,19 @@
 data "github_user" "current" {
   username = ""
 }
+
+resource "github_repository" "infrastructure" {
+  name        = "infrastructure"
+  description = "A repository to manage infrastructure for my personal projects"
+  topics      = ["terraform", "iac"]
+}
+
+resource "github_branch" "main" {
+  repository = github_repository.infrastructure.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "infrastructure" {
+  repository = github_repository.infrastructure.name
+  branch     = github_branch.main.branch
+}
