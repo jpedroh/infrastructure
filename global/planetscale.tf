6f487d21442439e13c5f6d01cd745c1396686c3d
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-09-04 20:21:32 -0300

    feat: import database branches

--- global/planetscale.tf
+++ global/planetscale.tf
@@ -5,16 +5,30 @@ resource "planetscale_database" "main" {
   notes        = ""
 }
 
+resource "planetscale_database_branch" "production" {
+  organization = planetscale_database.main.organization
+  database     = planetscale_database.main.name
+  name         = local.database_production_branch_name
+  region       = local.database_region
+}
+
+resource "planetscale_database_branch" "preview" {
+  organization = planetscale_database.main.organization
+  database     = planetscale_database.main.name
+  name         = local.database_preview_branch_name
+  region       = local.database_region
+}
+
 resource "planetscale_database_branch_password" "production" {
-  organization = local.planetscale_service_organization
-  database     = local.database_name
-  branch       = local.database_production_branch_name
+  organization = planetscale_database.main.organization
+  database     = planetscale_database.main.name
+  branch       = planetscale_database_branch.production.name
   name         = "${local.database_production_branch_name}-terraform"
 }
 
 resource "planetscale_database_branch_password" "preview" {
-  organization = local.planetscale_service_organization
-  database     = local.database_name
-  branch       = local.database_preview_branch_name
+  organization = planetscale_database.main.organization
+  database     = planetscale_database.main.name
+  branch       = planetscale_database_branch.preview.name
   name         = "${local.database_preview_branch_name}-terraform"
 }
