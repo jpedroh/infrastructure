6b60dbc90134eb85afc72d433acfd2e4285a4d04
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-09-04 20:26:42 -0300

    fix: add parent branch

--- global/planetscale.tf
+++ global/planetscale.tf
@@ -6,17 +6,19 @@ resource "planetscale_database" "main" {
 }
 
 resource "planetscale_database_branch" "production" {
-  organization = planetscale_database.main.organization
-  database     = planetscale_database.main.name
-  name         = local.database_production_branch_name
-  region       = local.database_region
+  organization  = planetscale_database.main.organization
+  database      = planetscale_database.main.name
+  name          = local.database_production_branch_name
+  region        = local.database_region
+  parent_branch = null
 }
 
 resource "planetscale_database_branch" "preview" {
-  organization = planetscale_database.main.organization
-  database     = planetscale_database.main.name
-  name         = local.database_preview_branch_name
-  region       = local.database_region
+  organization  = planetscale_database.main.organization
+  database      = planetscale_database.main.name
+  name          = local.database_preview_branch_name
+  region        = local.database_region
+  parent_branch = planetscale_database_branch.production.name
 }
 
 resource "planetscale_database_branch_password" "production" {
