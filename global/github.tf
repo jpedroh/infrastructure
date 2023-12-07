752283d916cd7692699dd5ce4e6f203824cdf777
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-06 22:21:38 -0300

    feat: Add branch restriction on main

--- global/github.tf
+++ global/github.tf
@@ -17,3 +17,15 @@ resource "github_branch_default" "infrastructure" {
   repository = github_repository.infrastructure.name
   branch     = github_branch.main.branch
 }
+
+resource "github_branch_protection" "require_terraform_cloud" {
+  repository_id = github_repository.infrastructure.node_id
+
+  pattern          = github_branch_default.infrastructure.branch
+  allows_deletions = false
+
+  required_status_checks {
+    strict   = true
+    contexts = ["Terraform Cloud/jpedroh/infrastructure"]
+  }
+}
