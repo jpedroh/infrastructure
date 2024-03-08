1633794fd9f0f5435380331f5e6b5b01313ffa0e
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-03-07 21:12:33 -0300

    feat: Remove PlanetScale configuration

--- reading-list/variables.tf
+++ reading-list/variables.tf
@@ -21,46 +21,6 @@ variable "otp_secret_dev_preview" {
   sensitive = true
 }
 
-variable "database_host_production" {
-  type      = string
-  sensitive = true
-}
-
-variable "database_username_production" {
-  type      = string
-  sensitive = true
-}
-
-variable "database_password_production" {
-  type      = string
-  sensitive = true
-}
-
-variable "database_url_production" {
-  type      = string
-  sensitive = true
-}
-
-variable "database_host_preview" {
-  type      = string
-  sensitive = true
-}
-
-variable "database_username_preview" {
-  type      = string
-  sensitive = true
-}
-
-variable "database_password_preview" {
-  type      = string
-  sensitive = true
-}
-
-variable "database_url_preview" {
-  type      = string
-  sensitive = true
-}
-
 variable "github_username" {
   type = string
 }
